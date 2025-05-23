672e7ccdf879a030b08801769c368e6c04a81b23
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2025-05-23 20:42:14 +0300

    update CreatePost component to handle image uploads and improve error handling

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,33 +1,59 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { useMutation } from "@tanstack/react-query"
 import { supabase } from "../supabase-client"
+
 
 
 interface PostInput {
     title: string;
     content: string;
 }
-const createPost = async (post: PostInput) => {
-    const {data, error} = await supabase.from("posts").insert(post);
-    
-    if (error) {
-        throw new Error(error.message);
-    }
+const createPost = async (post: PostInput, imageFile: File) => {
+  const filePath = `${post.title}-${Date.now()}-${imageFile.name}`;
 
-    return data;
+  const { error: uploadError } = await supabase.storage
+    .from("post-images")
+    .upload(filePath, imageFile);
+
+  if (uploadError) throw new Error(uploadError.message);
+
+  const { data: publicURLData } = supabase.storage
+    .from("post-images")
+    .getPublicUrl(filePath);
+
+  const { data, error } = await supabase
+    .from("posts")
+    .insert({ ...post, image_url: publicURLData.publicUrl });
+
+  if (error) throw new Error(error.message);
+
+  return data;
 };
 
 export const CreatePost = () => {
         const [title, setTitle] = useState<string>("");
         const [content, setContent] = useState<string>("");
+        const[selectedFile, setSelectedFile] = useState<File | null>(null);
 
-        const {mutate} = useMutation({mutationFn: createPost});
+        const {mutate} = useMutation({
+            mutationFn: (data: {post: PostInput, imageFile: File}) => {
+             return createPost(data.post, data.imageFile);
+        },
+    });
 
         const handleSubmit = (event: React.FormEvent) => {
             event.preventDefault();
-            mutate({title, content})
+            if(!selectedFile) return;
+            mutate({post: {title, content}, imageFile: selectedFile});
 
         };
+
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+        setSelectedFile(e.target.files[0]);
+        }
+    };
     return (  
         <form onSubmit={handleSubmit} >
             <div>
@@ -47,7 +73,16 @@ export const CreatePost = () => {
                   onChange={(event) => setContent(event.target.value)}
             />
             </div>
-
+            <div>
+                <label> upload image </label>
+                <input
+                  type="file" 
+                  id="image" 
+                  accept="image/*"
+                  required 
+                  onChange={handleFileChange}
+            />
+            </div>
             <button type="submit">create post</button>
         </form>
     );
