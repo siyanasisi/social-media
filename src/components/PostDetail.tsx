c7b581c58753a5c728fceea71c45296b8b0d978f
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2025-05-26 18:21:22 +0300

    Add CommentSection component for user comments in PostDetail

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { supabase } from "../supabase-client";
 import type { Post } from "./PostList";
 import { LikeButton } from "./LikeButton";
+import { CommentSection } from "./CommentSection";
 
 interface Props {
     postId: number;
@@ -44,6 +45,7 @@ export const PostDetail = ({ postId }: Props) => {
              posted on {new Date(data!.created_at).toLocaleDateString()}
         </p>
         <LikeButton postId={postId}/>
+        <CommentSection postId={postId} />
     </div>
     );
 }
