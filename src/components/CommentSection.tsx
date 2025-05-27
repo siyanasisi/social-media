8f1eddc4b8a60684b1276740140c541d8168a3cc
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2025-05-27 20:11:00 +0300

    Enhance CommentSection UI with improved styling for textarea and button, and add error message display

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -57,25 +57,34 @@ const createComment = async (
     
     return (
     <div className="mt-6">
-        <h3>
+        <h3 className="text-2xl font-semibold mb-4"> 
             Comments
         </h3>
         {user ? (
-            <form onSubmit={handleSubmit}>
+            <form 
+            onSubmit={handleSubmit}
+            className="mb-4"
+            >
                  <textarea
                   value={newCommentText}
                   rows={3} 
                   placeholder="write a comment..." 
                   onChange={(e) => setNewCommentText(e.target.value)}
+                  className="w-full border border-white/10 bg-transperent p-2 rounded"
                 />
-                 <button type="submit" disabled={!newCommentText}>
+                 <button 
+                    type="submit" 
+                    className="mt-2 bg-purple-500 text-white px-4 py-2 rounded cursor-pointer"
+                >
                     {isPending ? "Posting..." : "Post Comment"}
                    
                 </button>
-                {isError && <p className="text-red-500">Error posting comment</p>}
+                {isError && (
+                    <p className="text-red-500 mt-2">Error posting comment</p>
+                )}
             </form>
         ) : (
-            <p> please log in to comment</p>)}
+            <p className="mb-4 text-gray-600"> please log in to comment</p>)}
 
     </div>
     );
