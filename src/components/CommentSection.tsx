c9255a03cb792c1df2af0a1dfd77fd169a56e3db
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2025-05-27 20:04:40 +0300

    Implement comment creation functionality in CommentSection component

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -1,23 +1,62 @@
 import { useAuth } from "../context/AuthContext"
 import { useState } from "react";
-
+import { useMutation } from "@tanstack/react-query";
+import { supabase } from "../supabase-client";
 interface Props {
     postId: number;
 }
 
+interface NewComment {
+    content: string;
+    parent_comment_id?: number | null;
+}
+
+const createComment = async (
+    newComment: NewComment, 
+    postId: number, 
+    userId?: string, 
+    author?: string
+) => {
+    if(!userId || !author) throw new Error("User must be logged in to comment");
+    const { error } = await supabase
+        .from("comments").insert({
+            post_id: postId,
+            content: newComment.content,
+            user_id: userId,
+            parent_comment_id: newComment.parent_comment_id || null,
+            author: author
+        });
+    if (error) throw new Error(error.message);  
+
+    
+}
+
  export const CommentSection = ({ postId }: Props) => {
-      const[newCommentText, setNewCommentText] = useState<string>("");
+    const[newCommentText, setNewCommentText] = useState<string>("");
     const { user } = useAuth();
+
+    const { mutate, isPending, isError } = useMutation({
+        mutationFn:(newComment: NewComment) => 
+            createComment(
+                newComment,
+                postId, 
+                user?.id,
+                user?.user_metadata.user_name
+        ),
+    }) 
+
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
 
         if(!newCommentText) return;
 
+        mutate({ content: newCommentText, parent_comment_id: null});
+        setNewCommentText("");
     }
     
     return (
-    <div>
+    <div className="mt-6">
         <h3>
             Comments
         </h3>
@@ -29,7 +68,11 @@ interface Props {
                   placeholder="write a comment..." 
                   onChange={(e) => setNewCommentText(e.target.value)}
                 />
-                 <button type="submit" disabled={!newCommentText}> Post Comment </button>
+                 <button type="submit" disabled={!newCommentText}>
+                    {isPending ? "Posting..." : "Post Comment"}
+                   
+                </button>
+                {isError && <p className="text-red-500">Error posting comment</p>}
             </form>
         ) : (
             <p> please log in to comment</p>)}
