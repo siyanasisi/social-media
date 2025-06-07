76f76679b619ebd8cf4ed28006221b9cd6842273
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2025-06-07 07:52:59 +0300

    Implement reply functionality in CommentItem with error handling and loading state

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,5 +1,9 @@
 import type { Comment } from "./CommentSection";
 import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useMutation } from "@tanstack/react-query";
+import { supabase } from "../supabase-client";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface Props {
     comment: Comment & {
@@ -7,9 +11,60 @@ interface Props {
     };
     postId: number;
 }
+const createReply = async (
+    replyContent: string, 
+    postId: number, 
+    parentCommentId: number, 
+    userId?: string, 
+    author?: string
+) => {
+
+    if(!userId || !author) throw new Error("User must be logged in to reply");
+       
+    const { error } = await supabase
+            .from("comments").insert({
+                post_id: postId,
+                content: replyContent,
+                user_id: userId,
+                parent_comment_id: parentCommentId,
+                author: author
+            });
+        if (error) throw new Error(error.message);  
+    
+
+}
 
 export const CommentItem = ({comment, postId}: Props) => {
     const [showReply, setShowReply] = useState<boolean>(false);
+    const [replyText, setReplyText] = useState<string>("");
+
+    const { user } = useAuth();
+    const queryClient = useQueryClient();
+    
+    const { mutate, isPending, isError } = useMutation({
+        mutationFn:(replyContent: string) => 
+            createReply(
+                replyContent,
+                postId, 
+                comment.id,
+                user?.id,
+                user?.user_metadata.user_name
+        ),
+        onSuccess: () => {
+          queryClient.invalidateQueries({ queryKey: ["comments", postId]})
+            setReplyText("");
+            setShowReply(false);
+        },
+    }) 
+
+    const handleReplySubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+
+        if(!replyText) return;
+
+        mutate(replyText);
+      
+    };
     return (
     <div>
         <div>
@@ -19,8 +74,35 @@ export const CommentItem = ({comment, postId}: Props) => {
                <span>{new Date(comment.created_at).toLocaleString()}</span>
             </div>
             <p>{comment.content}</p>
-            <button onClick={() => setShowReply((prev) => !prev)}>{showReply ? "Cancel" : "Reply"}</button>
+            <button onClick={() => setShowReply((prev) => !prev)}>
+                {showReply ? "Cancel" : "Reply"}
+            </button>
         </div>
+
+        {showReply && user && (
+            <form 
+            onSubmit={handleReplySubmit}
+            className="mb-4"
+            >
+                 <textarea
+                  value={replyText}
+                  rows={2} 
+                  placeholder="write a reply..." 
+                  onChange={(e) => setReplyText(e.target.value)}
+                  className="w-full border border-white/10 bg-transperent p-2 rounded"
+                />
+                 <button 
+                    type="submit" 
+                    className="mt-2 bg-purple-500 text-white px-4 py-2 rounded cursor-pointer"
+                >
+                    {isPending ? "Posting..." : "Post Reply"}
+                   
+                </button>
+                {isError && (
+                    <p className="text-red-500 mt-2">Error posting reply</p>
+                )}
+            </form>
+        )}
 
     </div>
     )
