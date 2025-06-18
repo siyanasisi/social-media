f60a8c1187f8cfbb8f465d1f43df150d2c266d56
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2025-06-19 02:46:55 +0300

    Add ESLint configuration for code quality and consistency + like button tests

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y", "import"],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
