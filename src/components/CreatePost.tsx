af8a6d2d26aefcb4517be6a4954d153517f89d07
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2025-06-19 01:51:19 +0300

    Refactor imports in CreatePost component for clarity

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent,  useState } from "react";
+import type { ChangeEvent } from "react";
+import { useState } from "react";
 import { useMutation } from "@tanstack/react-query"
 import { supabase } from "../supabase-client"
 import { useAuth } from "../context/AuthContext";
